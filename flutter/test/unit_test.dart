0de83242296a6f2b2ababaae924b3a1b2cfbc7e8
iori tobeta <15eb1137@gmail.com>
2023-05-22 07:04:02 +0900

    [test green] アカウントのissueBalanceNowが正しく計算できる

--- flutter/test/unit_test.dart
+++ flutter/test/unit_test.dart
@@ -19,38 +19,52 @@ void main() {
       expect(targetDate3.isAfter(today), true);
     });
   });
-  test('ユーザーは、プレミアムにアップデートされて7日後になるとPremiumStateがExpiredになる', () {
-    final user = User.create();
-    final today = DateTime(2023, 1, 1, 0, 0, 0, 0, 0);
-    final updatedUser = user.updateToPremium(updatedAt: today);
+  group('Small tests', () {
+    test('ユーザーは、プレミアムにアップデートされて7日後になるとPremiumStateがExpiredになる', () {
+      final user = User.create();
+      final today = DateTime(2023, 1, 1, 0, 0, 0, 0, 0);
+      final updatedUser = user.updateToPremium(updatedAt: today);
 
-    final stillPremiumAt = DateTime(2023, 1, 8, 0, 0, 0, 0, 0);
-    expect(updatedUser.isPremiumExpiredWhen(stillPremiumAt), false);
+      final stillPremiumAt = DateTime(2023, 1, 8, 0, 0, 0, 0, 0);
+      expect(updatedUser.isPremiumExpiredWhen(stillPremiumAt), false);
 
-    final expiredAt = DateTime(2023, 1, 8, 0, 0, 0, 0, 1);
-    expect(updatedUser.isPremiumExpiredWhen(expiredAt), true);
-  });
-  test('アカウントのトランザクションには現時点より過去の日付しか含まれない', () {
-    final account = Account.create('80ae0478-a252-415f-b34e-b1b515ec4855');
-    final today = DateTime(2023, 1, 1, 0, 0, 0, 0, 0);
-    final targetDays = [
-      DateTime(2022, 12, 31, 0, 0, 0, 0, 0),
-      DateTime(2023, 1, 1, 0, 0, 0, 0, 0),
-      DateTime(2023, 1, 2, 0, 0, 0, 0, 0)
-    ];
-    final hasTransactionsAccount0 = account.addTransactionHistory(targetDays[0], today);
-    final hasTransactionsAccount1 = hasTransactionsAccount0.addTransactionHistory(targetDays[1], today);
-
-    expect(() => hasTransactionsAccount1.addTransactionHistory(targetDays[2], today), throwsException);
-    
-    final isAccountTransactionsDateBeforeNow = hasTransactionsAccount1.transactions.children.every((transaction) {
-      final transactionAt = transaction.transactionAt.value;
-      return transactionAt.isBefore(today) || transactionAt.isAtSameMomentAs(today);
+      final expiredAt = DateTime(2023, 1, 8, 0, 0, 0, 0, 1);
+      expect(updatedUser.isPremiumExpiredWhen(expiredAt), true);
     });
-    expect(isAccountTransactionsDateBeforeNow, true);
+    test('アカウントのトランザクションには現時点より過去の日付しか含まれない', () {
+      final account = Account.create('80ae0478-a252-415f-b34e-b1b515ec4855');
+      final today = DateTime(2023, 1, 1, 0, 0, 0, 0, 0);
+      final targetDays = [
+        DateTime(2022, 12, 31, 0, 0, 0, 0, 0),
+        DateTime(2023, 1, 1, 0, 0, 0, 0, 0),
+        DateTime(2023, 1, 2, 0, 0, 0, 0, 0)
+      ];
+      final hasTransactionsAccount0 = account.addTransactionHistory(targetDays[0], today);
+      final hasTransactionsAccount1 = hasTransactionsAccount0.addTransactionHistory(targetDays[1], today);
+
+      expect(() => hasTransactionsAccount1.addTransactionHistory(targetDays[2], today), throwsException);
+
+      final isAccountTransactionsDateBeforeNow = hasTransactionsAccount1.transactions.children.every((transaction) {
+        final transactionAt = transaction.transactionAt.value;
+        return transactionAt.isBefore(today) || transactionAt.isAtSameMomentAs(today);
+      });
+      expect(isAccountTransactionsDateBeforeNow, true);
+    });
+    test('アカウントのissueBalanceNowが正しく計算できる', () {
+      final account = Account.create('80ae0478-a252-415f-b34e-b1b515ec4855');
+      final today = DateTime(2023, 1, 1, 0, 0, 0, 0, 0);
+      final targetDays = [DateTime(2022, 12, 29, 0, 0, 0, 0, 0), DateTime(2022, 12, 30, 0, 0, 0, 0, 0)];
+      final hasTransactionsAccount = account
+          .changeBalance(500)
+          .addTransactionHistory(targetDays[0], today)
+          .addTransactionHistory(targetDays[1], today);
+      final amountChangedTransactionsAccount = hasTransactionsAccount
+          .changeTransactionHistory(hasTransactionsAccount.transactions.children[0].id.value, false, newAmount: 200)
+          .changeTransactionHistory(hasTransactionsAccount.transactions.children[1].id.value, false, newAmount: -100);
+      final balanceValue = amountChangedTransactionsAccount.issueBalanceNow().value;
+      expect(balanceValue, 600);
+    });
+    test('ShortageのMessageが正しく組み立てられる', () {});
+    test('未来の日付のbalanceが正しく計算できる', () {});
   });
-  test('アカウントのバランスのdateは現時点より過去の日付になる', () {});
-  test('アカウントのissueBalanceNowが正しく計算できる', () {});
-  test('ShortageのMessageが正しく組み立てられる', () {});
-  test('未来の日付のbalanceが正しく計算できる', () {});
 }

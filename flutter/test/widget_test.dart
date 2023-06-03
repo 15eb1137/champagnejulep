bd5910986d979fa8470e6937831e1d330b24ef61
iori tobeta <15eb1137@gmail.com>
2023-06-04 01:33:23 +0900

    add empty widget test

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('[Learning tests]', () {});
+  group('[Learning tests]', () {
+    testWidgets('', (tester) async {});
+  });
 }
